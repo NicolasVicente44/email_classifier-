34bee8a9abb9a39cc431c95454fe26b1ba64737d
NicolasVicente44 <nicolas.a.vicente44@gmail.com>
2024-10-03 09:51:26 -0400

    modified features functions to better reflect data and use more composite classes

--- NicolasVicente_A1.py
+++ NicolasVicente_A1.py
@@ -11,15 +11,13 @@ from sklearn.metrics import accuracy_score
 try:
     data = pd.read_csv("email.tsv", sep="\t")
 except FileNotFoundError:
-    print("the file 'email.tsv' was not found.")
+    print("the tsv file 'email.tsv' was not found")
     exit()
 print(data.columns)
 
 
 def spam_keywords(row):
-    viagra_present = row["viagra"]
-    winner_present = 1 if row["winner"] == "yes" else 0
-    return viagra_present + winner_present
+    return row["viagra"] + (1 if row["winner"] == "yes" else 0) + row["inherit"]
 
 
 def content_density(row):
@@ -31,39 +29,46 @@ def content_density(row):
 
 
 def sender_credibility(row):
-    email = row["from"]
-    if isinstance(email, str):
-        return 1 if email.endswith("noreply") or email.endswith(".com") else 0
-    return 0
+    return row["from"] + row["to_multiple"] + row["sent_email"]
 
 
 def urgency_indicator(row):
-    return row["urgent_subj"] + row["exclaim_subj"] + (1 if row["dollar"] > 0 else 0)
+    return (
+        row["exclaim_subj"]
+        + row["urgent_subj"]
+        + row["exclaim_subj"]
+        + (1 if row["dollar"] > 0 else 0)
+        + (1 if row["password"] > 0 else 0)
+    )
 
 
-def password_mentioned(row):
-    return row["password"]
+def financial_spam_indicator(row):
+
+    return (
+        (1 if row["dollar"] > 0 else 0)
+        + (1 if row["number"] != "none" else 0)
+        + (1 if row["attach"] > 0 else 0)
+    )
 
 
 def multiple_recipients(row):
-    return 1 if row["to_multiple"] > 1 else 0
+    return row["to_multiple"] + 1 if row["cc"] > 0 else 0
 
 
 data["spam_keywords"] = data.apply(spam_keywords, axis=1)
 data["content_density"] = data.apply(content_density, axis=1)
 data["sender_credibility"] = data.apply(sender_credibility, axis=1)
 data["urgency_indicator"] = data.apply(urgency_indicator, axis=1)
-data["password_mentioned"] = data.apply(password_mentioned, axis=1)
+data["financial_spam_indicator"] = data.apply(financial_spam_indicator, axis=1)
 data["multiple_recipients"] = data.apply(multiple_recipients, axis=1)
 
-data["time"] = pd.to_datetime(data["time"]).astype(int) / 10**9
 
 feature_columns = [
     "spam_keywords",
     "content_density",
     "sender_credibility",
     "urgency_indicator",
-    "password_mentioned",
+    "financial_spam_indicator",
     "multiple_recipients",
 ]
 
@@ -114,7 +119,3 @@ classifierSVM = svm.LinearSVC()
 classifierSVM.fit(X_train, y_train)
 SVM_predictions = classifierSVM.predict(X_test)
 print("SVM Test set score: {:.2f}".format(accuracy_score(y_test, SVM_predictions)))
-
-
-
-
